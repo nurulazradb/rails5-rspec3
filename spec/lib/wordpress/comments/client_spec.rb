1264d22a62af3b027c0ead3e76e88715a90be479
Nurulazrad Murad <nurulazrad@gmail.com>
2017-02-15 15:11:42 +0800

    Parse XML

--- spec/lib/wordpress/comments/client_spec.rb
+++ spec/lib/wordpress/comments/client_spec.rb
@@ -1,3 +1,5 @@
+require 'nokogiri'
+
 module Wordpress
   module Comments
     class Client
@@ -6,16 +8,45 @@ module Wordpress
       def initialize url
         @url = url
       end
+
+      def parse xml
+        doc = Nokogiri::XML xml
+        doc.search('item').map do |doc_item|
+          item = {}
+          item[:link] = doc_item.at('link').text
+          item
+        end
+      end
     end
   end
 end
 
 RSpec.describe Wordpress::Comments::Client do
 
-  it "stores a URL" do
-    client = Wordpress::Comments::Client.new 'http://mashable.com/comments/feed'
+  describe "#initialize" do
 
-    expect(client.url).to eq 'http://mashable.com/comments/feed'
+    it "stores a URL" do
+      client = Wordpress::Comments::Client.new 'http://feeds.mashable.com/mashable/tech'
+
+      expect(client.url).to eq 'http://feeds.mashable.com/mashable/tech'
+    end
+
+  end
+
+  describe "#parse" do
+
+    # let(:xml) { file_fixture("feed.xml").read }
+    let(:xml) { File.read(File.join('spec', 'fixtures', 'files', 'feed.xml'))}
+
+    it "extracts the link" do
+      client = Wordpress::Comments::Client.new 'http://feeds.mashable.com/mashable/tech'
+      comments = client.parse xml
+      comment = comments.first
+      link = 'http://feeds.mashable.com/~r/mashable/tech/~3/H19a27DsGe8/'
+
+      expect(comment[:link]).to eq link
+    end
+
   end
 
 end
